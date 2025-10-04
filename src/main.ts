a41d2c15a645795d96add63cff821fa08c45287a
ThienDh <dinhhuuthien99@gmail.com>
2025-10-04 12:54:27 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const configService = app.get(ConfigService);
+
   const port = configService.get<number>('PORT') ?? 3000;
 
+  app.enableCors({
+    origin: 'http://localhost:5173', // hoặc URL FE của bạn
+    credentials: true, // nếu bạn muốn gửi cookie/token kèm request
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
